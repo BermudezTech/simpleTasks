6a424c96d6ae4a69d9bbe8465e2f96c9f685c098
JBermudezG <jebermudez587@gmail.com>
2023-12-12 17:45:51 -0500

    Changed conexion config

--- conexion.php
+++ conexion.php
@@ -1,4 +1,4 @@
-<?php 
-	$conexion=new PDO('mysql:host=localhost;dbname=mis_tareas','root','');
-	$conexion->exec('set names utf8');
-?>
+<?php
+
+$conexion = new PDO('mysql:host=db;dbname=mis_tareas', 'root', 'password');
+$conexion->exec('set names utf8');
